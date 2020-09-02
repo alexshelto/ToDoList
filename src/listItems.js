a31f2e229e961f5c133ae476aa98de248f549ad7
alexshelto <alexshelto@icloud.com>
2020-09-01 22:26:31 -0400

    Front end work done

--- src/listItems.js
+++ src/listItems.js
@@ -9,17 +9,16 @@ import './list-items.css';
 function ListItems(props) {
   const items = props.items;
   const list_items = items.map(item => {
-    return <div className="ListItems" key={item.key}>
-      <p>
-        <input type="text" id={item.key} value={item.ToDo} onChange={(e)=> {
-          props.setUpdate(e.target.value, item.key)}} />
+    return <div className="ListItems" key={item.key}> 
+               <p>
+                  <input type="text" id={item.key} value={item.ToDo} onChange={(e)=> {
+                  props.setUpdate(e.target.value, item.key)}} />
         
-          <span>
-              <FontAwesomeIcon className="faicons" onClick={()=>{
-              props.deleteItem(item.key) }} icon="trash" />
-          </span>
-      </p>
-        
+               <span>
+                  <FontAwesomeIcon className="faicons" onClick={()=>{
+                  props.deleteItem(item.key) }} icon="trash" />
+              </span>
+          </p>
         </div>})
 
 
